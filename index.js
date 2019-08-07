ea9f51b33cd82dd8f85eca3c90522870c6ab8498
James Long <longster@gmail.com>
2019-08-07 15:55:10 -0400

    Fix run script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function run() {
   // let budgets = await findBudgets();
 
   let filepath = process.argv[2];
-  await importYNAB4(budgets[0].filepath);
+  await importYNAB4(filepath);
 }
 
 run();

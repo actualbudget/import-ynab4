6ad9babe15b64a333dddd367f3c9c2961e0eeab3
James Long <longster@gmail.com>
2019-08-07 15:45:20 -0400

    Add command

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { findBudgets, importYNAB4 } = require('./importer');
 
 async function run() {
-  let budgets = await findBudgets();
+  // let budgets = await findBudgets();
+
+  let filepath = process.argv[2];
   await importYNAB4(budgets[0].filepath);
 }
 
